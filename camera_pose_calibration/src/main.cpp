770d5203432d48c1d2f40444ebacc3f8cf853f29
Ronald Ensing <r.m.ensing@delftrobotics.com>
2015-12-24 19:01:01 +0100

    Display when node is initialized.

--- camera_pose_calibration/src/main.cpp
+++ camera_pose_calibration/src/main.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char * argv[]) {
 	ros::init(argc, argv, ROS_PACKAGE_NAME);
-
 	camera_pose_calibration::CameraPoseCalibrationNode node;
+	ROS_INFO_STREAM(ROS_PACKAGE_NAME << " node initialized.");
 	ros::spin();
 }
